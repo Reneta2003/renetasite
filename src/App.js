2acc531067b74d12aed3f8426de322d8c31613cd
Reneta2003 <r.trifonova@student.utwente.nl>
2024-02-14 13:35:50 +0100

    Inital commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>Hello my beautiful <span style={{color: 'pink'}}>fairy</span> <span style={{color: 'red'}}>Rennie</span></div>;
 }
 
 export default App;
